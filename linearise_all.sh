91b4edc31dd33886d30aa41fcb0a117d6de84a41
Simon Will <will@cl.uni-heidelberg.de>
2021-01-23 18:13:20 +0100

    Update mass linearise script to import mrl via nlmaps_tools

--- linearise_all.sh
+++ linearise_all.sh
@@ -4,5 +4,5 @@ find . -name '*.mrl' |
     while read -r mrlfile; do
         linfile="${mrlfile%mrl}lin"
         echo "$mrlfile -> $linfile"
-        python -m mrl.linearise -i "$mrlfile" -o "$linfile"
+        python -m nlmaps_tools.mrl.linearise -i "$mrlfile" -o "$linfile"
     done
